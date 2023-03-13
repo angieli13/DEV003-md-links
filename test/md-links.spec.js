ccbd69a060cb09f6c5c50729c4523383b3758112
angieli13 <angiequimica13@gmail.com>
2023-03-12 21:28:56 -0500

    configuración package.json y función mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
@@ -6,5 +6,7 @@ describe('mdLinks', () => {
   it('should...', () => {
     console.log('FIX ME!');
   });
-
+  it('Debería devolver una promesa', () => {
+    expect(mdLinks()).tobe(typeof Promise);
+  });
 });
