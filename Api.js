4adf02d1ed78082cd0afbf32b5d1fd8c0c86d0ce
angieli13 <angiequimica13@gmail.com>
2023-03-18 21:25:24 -0500

    1° borrador de la función mdLinks a partir de Api

--- Api.js
+++ Api.js
@@ -12,7 +12,7 @@ const isAbsolute = route => path.isAbsolute(route);
 //console.log('¿Es absoluto?',isAbsolute('README.md'));
 
 //Convertir el path en Absoluto 
-const toAbsolutePath = route => path.resolve(route);
+const toAbsolutePath = absolutePath => path.resolve(absolutePath);
 //console.log('Ruta relativa convertida',toAbsolutePath(isAbsolute('README.md')));
 
 // Verificar si es archivo
@@ -20,15 +20,15 @@ const isFile = filePath => fs.statSync(filePath).isFile();
 //console.log('¿Es archivo?',isFile("C:/Users/yilib/Documents/ProyectosLAB/DEV003-md-links"));
 
 //verifico si es un directorio 
-const isDirectory = (path) => fs.statSync(path).isDirectory();
+const isDirectory = path => fs.statSync(path).isDirectory();
 //console.log(isDirectory('C:/Users/yilib/Documents/ProyectosLAB/DEV003-md-links'))
 
 //Buscar el archivo dentro del directorio
-const searchFiles = filePath => fs.readdirSync(filePath);
+const searchFiles = path => fs.readdirSync(path);
 //console.log(Searchfiles(isDirectory('C:/Users/yilib/Documents/ProyectosLAB/DEV003-md-links')))
 
 // Identificar si el archivo tiene extensión .md, los filtro y los retorno en un array
-const mdFiles = filePath => fs.readdirSync(filePath)
+const mdFiles = path => fs.readdirSync(path)
     .filter (file => path.extname(file) === '.md');
 //console.log('MD', mdFiles('C:/Users/yilib/Documents/ProyectosLAB/DEV003-md-links'))
 
@@ -37,15 +37,15 @@ const readMd = fileName => fs.readFileSync(fileName, 'utf-8');
 //console.log(readMd(mdFiles(prueba)))
 
 //Crear función para extraer los links (http) de los archivos .md tener encuenta si tiene carpetas llamar funcion dentro de la función
-const extractLinks = (mdContent) => {
-  const regex = /\[(.+)\]\((http[s]?:\/\/.+)\)/g;
-  const links = [];
+const extractLinks = (mdContent) => {//Toma como parámetro una cadena de texto en formato Markdown.
+  const regEx = /\[(.+)\]\((http[s]?:\/\/.+)\)/g;//Expresión regular para buscar patrones que corresponden a la sintaxis de un enlace en Markdown,(.+) corresponde al texto del enlace y otro grupo de captura que corresponde a la dirección URL del enlace.
+  const links = []; //Arreglo vacío que almacenará los enlaces encontrados.
 
-  let match;
-  while ((match = regex.exec(mdContent)) !== null) {
-    links.push({
-      text: match[1],
-      href: match[2],
+  let match;//Almacena el resultado de cada búsqueda de coincidencias.
+  while ((match = regEx.exec(mdContent)) !== null) {//Devuelve un arreglo que contiene información sobre la primera coincidencia encontrada en la cadena de texto. Si no se encuentra ninguna coincidencia, el método devuelve null.
+    links.push({//Dentro del ciclo while, se agrega un nuevo objeto al arreglo links, que contiene dos propiedades text y href.
+      text: match[1], //contiene el texto del enlace
+      href: match[2],//contiene la URL del enlace
     });
   }
 
--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 const Api = require('./Api.js');
 
 const mdLinks = (path, options) => {
-  // console.log(mdLinks(path, options));
   return new Promise((resolve, reject) => {
-    if (!Api.isPathValid(path) ) {
+    if (!Api.isPathValid(path)) {
       reject(`La ruta especificada no es valida: ${path}`);
-    };
-    if (Api.isAbsolute(path)){
-      reject(`La ruta especificada no es absoluta ${path}`);
+    } else if (Api.isAbsolute(path)) {
+      reject(`La ruta especificada no es relativa: ${path}`);
+    } else if (!Api.isDirectory(path) && !Api.isFile(path)) {
+      reject(`La ruta especificada no es un archivo: ${path}`);
+    } else {
+      const absolutePath = Api.toAbsolutePath(path);
+      let mdFiles = [];
+      if (Api.isDirectory(absolutePath)) {
+        mdFiles = Api.mdFiles.searchFiles(absolutePath).map(file => path.join(absolutePath, file));
+      } else {
+        mdFiles = [absolutePath];
+      }
+      const links = [];
+      mdFiles.forEach(mdFile => {
+        const mdContent = Api.readMd(mdFile);
+        const fileLinks = extractLinks(mdContent);
+        fileLinks.forEach(link => {
+          links.push({
+            file: mdFile,
+            text: link.text,
+            href: link.href
+          });
+        });
+      });
+      resolve(links);
     }
-    
-    resolve('Resultado')
+  });
+};
 
-  )}
-  
-}
-
-
-
-
-
-
-module.exports = { mdLinks };
 mdLinks('C:/Users/yilib/Documents/ProyectosLAB/DEV003-md-links/README.md').then((result) => {
   console.log(result);
 
@@ -29,3 +40,5 @@ mdLinks('C:/Users/yilib/Documents/ProyectosLAB/DEV003-md-links/README.md').then(
   .catch((Error) => {
     console.log(Error)
   });
+
+module.exports = { mdLinks };
