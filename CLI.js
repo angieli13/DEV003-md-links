869d55d44f4a683fe475381321439a0e1441df15
angieli13 <angiequimica13@gmail.com>
2023-04-01 01:11:18 -0500

    CLI terminado

--- CLI.js
+++ CLI.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const { mdLinks } = require('./index');
 const {
- totalLinks, uniqueLinks, brokenLinks,
+  totalLinks, uniqueLinks, brokenLinks,
 } = require('./Api.js');
-const process =require('process');
+const process = require('process');
 var colors = require('colors/safe');
 const emoji = require('node-emoji');
 const argv = process.argv;//Asigna los argumentos de línea de comando
@@ -11,68 +11,73 @@ const path = process.argv[2];
 console.log(process.argv)
 
 
-function cli (){
-  const validate = argv.includes('--validate')|| argv.includes('--v');
-  const stats = argv.includes('--stats')|| argv.includes('--s');
-  const help = argv.includes('--help')|| argv.includes('--h');
-  
+function cli() {
+  const validate = argv.includes('--validate') || argv.includes('--v');
+  const stats = argv.includes('--stats') || argv.includes('--s');
+  const help = argv.includes('--help') || argv.includes('--h');
 
- if (path === undefined || path === help ){
-  console.log((colors.rainbow('\n-------------------------------------------------------------\n                    Welcome to AngieLi MD-links')) + emoji.get('👩🏻‍💻') + (colors.rainbow( '\n-------------------------------------------------------------' )));
-  console.log(colors.bgMagenta('\n🔎Instructions:'));
-        console.log(colors.white('1. Submit a valid path right after "AngieLi-md-links".'));
-        console.log(colors.white('2. To see links route, url, status message, status number and text, write "--validate" or "--v" right after your path.'));
-        console.log(colors.white('3. To see total links number and unique links number, write "--stats" or "--s" right after your path.'));
-        console.log(colors.white('4. To additionally see broken links number, add "--stats --validate" or "--s --v" right after your path.'));
-        console.log(colors.white('5. To see these instructions again, you can type "--help" or "--h" right after "AngieLi-md-links".'));
-        console.log(colors.rainbow('======================================================================================================='));
+
+  if (path === undefined || path === "--help" || path === "--h") {
+    console.log((colors.rainbow('\n-------------------------------------------------------------\n                    Welcome to AngieLi MD-links')) + "👩🏻‍💻" + (colors.rainbow('\n-------------------------------------------------------------')));
+    console.log(colors.bgMagenta('\n🔎Instructions:'));
+    console.log(colors.magenta('1.') + colors.white(' A valid path right after') + colors.blue('  AngieLi-md-links.'));
+    console.log(colors.magenta('2.') + colors.white(' To see links route, url, status message, status number and text, write') + colors.blue('  --validate') + colors.white(' or') + colors.blue('  --v') + colors.white(' right after your path.'));
+    console.log(colors.magenta('3.') + colors.white(' To see total links number and unique links number, write ') + colors.blue('  --stats') + colors.white(' or') + colors.blue('  --s') + colors.white(' right after your path.'));
+    console.log(colors.magenta('4.') + colors.white(' To additionally see broken links number, add') + colors.blue('  --stats --validate') + colors.white(' or') + colors.blue('  --s --v') + colors.white(' right after your path.'));
+    console.log(colors.magenta('5.') + colors.white(' To see these instructions again, you can type') + colors.blue('  --help') + colors.white(' or') + colors.blue('  --h') + colors.white(' right after') + colors.blue('  AngieLi-md-links.'));
+    console.log(colors.rainbow('======================================================================================================='));
+
     process.exit(0);//para salir de la aplicación con éxito.
- }
- else if (stats && validate){
-  return mdLinks(path, { validate: validate, stats }).then((links) => {
-    console.log(colors.bgMagenta('\nTOTAL LINKS  :')+(colors.bgBlue(totalLinks(links))));
-    console.log(colors.bgMagenta('\nUNIQUE LINKS :')+(colors.bgBlue(uniqueLinks(links))));
-    console.log(colors.bgMagenta('\nBROKEN LINKS :')+(colors.bgBlue(brokenLinks(links))));
-  }).catch((error) => { console.log(error); })
-}else if (stats && !validate){
+  }
+  else if (stats && validate) {
+    return mdLinks(path, { validate: validate, stats }).then((links) => {
+      console.log(colors.bgMagenta('\nTOTAL LINKS  :') + (colors.bgBlue(totalLinks(links))));
+      console.log(colors.bgMagenta('\nUNIQUE LINKS :') + (colors.bgBlue(uniqueLinks(links))));
+      console.log(colors.bgMagenta('\nBROKEN LINKS :') + (colors.bgBlue(brokenLinks(links))));
+      console.log((colors.rainbow('\n================================= AngieLi MD-links')) + "👩🏻‍💻" + (colors.rainbow('=================================')));
+      process.exit(0);//para salir de la aplicación con éxito.
+    }).catch((error) => { console.log(error); })
 
- }
-  else if (!stats && validate){
+  } else if (stats && !validate) {
+    return mdLinks(path, { validate: stats }).then((links) => {
+      console.log(colors.bgMagenta('\nTOTAL LINKS  :') + (colors.bgBlue(totalLinks(links))));
+      console.log(colors.bgMagenta('\nUNIQUE LINKS :') + (colors.bgBlue(uniqueLinks(links))));
+      console.log((colors.rainbow('\n================================= AngieLi MD-links')) + "👩🏻‍💻" + (colors.rainbow('=================================')));
+      process.exit(0);
+    }).catch((error) => { console.log(error); })
 
- }
- else{
+  }
+  else if (!stats && validate) {
+    return mdLinks(path, { validate: validate }).then((links) => {
+      console.log(colors.rainbow('\nVALIDACION DE LINKS'+ "✍️"));
+      const brokenLinks = links.filter(link => link.statusText !== 'OK');
+      if (brokenLinks.length > 0) {
+        console.log(colors.red('\nENLACES ROTOS:') + (colors.bgRed('  ' + brokenLinks.length)));
+        brokenLinks.forEach(link => {
+          console.log(colors.bgMagenta('\nHREF :') + (colors.bgBlue(link.href)));
+          console.log(colors.bgMagenta('\nFILE :') + (colors.bgBlue(link.file)));
+          console.log(colors.bgMagenta('\nSTATUS :') + (colors.bgRed(link.status)));
+          console.log(colors.bgMagenta('\nTEXT :') + (colors.bgBlue(link.text)));
+          });
+          console.log((colors.rainbow('\n================================= AngieLi MD-links')) + "👩🏻‍💻" + (colors.rainbow('=================================')));
+        process.exit(1);
+      } else {
+        console.log(colors.bgGreen('Todos los enlaces funcionan correctamente.'));      
+        console.log((colors.rainbow('\n================================= AngieLi MD-links')) + "👩🏻‍💻" + (colors.rainbow('=================================')));  
+        process.exit(0);
+      }
+    }).catch((error) => { console.log(error); })
+  }
 
- }
+  else {
+    console.log(colors.yellow('This invalid command, if you need help use the command --help or --h'));
+    console.log((colors.rainbow('\n================================= AngieLi MD-links')) + "👩🏻‍💻" + (colors.rainbow('=================================')));
+
+  }
 };
-
 
 cli()
 
-/*const options = {
-  validate: process.argv.includes('--validate'),
-  };
-  const stats = process.argv.includes('--stats');
-
-/*mdLinks(path, options)//llamo mi funcion en cada if
-  .then((links) => {
-    console.log(links);
-    if (stats && options.validate){//¿Qué va imrimir mi programa?
-      /*$ md-links ./some/example.md --stats --validate
-Total: 3
-Unique: 3
-Broken: 1*/
-       
-   /* }else if(stats){
-
-    }else if (options.validate){
-       
-    }else {
-
-    }
-  })
-  .catch((err) => {
-    console.error(err.message);
-  });*/
 
 
 
